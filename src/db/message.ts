494d80d2d6bc2c237cb4bf4ce0f456502eaf72be
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-24 07:55:03 +0200

    added message type

--- src/db/message.ts
+++ src/db/message.ts
@@ -1,10 +1,12 @@
 import { unstable_cache } from "next/cache"
 import { prisma } from "./prisma"
 import { cache } from "react"
+import { Message } from "@prisma/client"
 
 
 
-export const createMessage = async ({ id, title, memberId, channelId, replyTo }: MessageInterFace) => {
+
+export const createMessage = async ({ id, title, memberId, channelId, replyTo }: Message) => {
     const data = await prisma.message.create({
         data: {
             id,
