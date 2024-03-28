21584acbe4d44d1af92cf74b720c0580d64630a7
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-28 04:56:36 +0200

    some changes in the route

--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -1,13 +1,13 @@
 import { createUser } from "@/db/user";
-import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest) => {
+export const POST = async (req: NextRequest): Promise<Response> => {
     try {
-        const data = await req.json()
-        await createUser(data)
-        return Response.json({ data }, { status: 200 })
+        const data = await req.json();
+        const newUser = await createUser(data);
+        return Response.json({ newUser }, { status: 200 });
     } catch (error) {
-        return new NextRequest(`[CREATE_USER_ERR] ${error}`,)
+        const errorMessage = `[CREATE_USER_ERR] ${error}`;
+        return new NextResponse(errorMessage);
     }
-}
+};
--- src/app/auth/message/route.ts
+++ src/app/auth/message/route.ts
@@ -1,13 +1,13 @@
 import { createMessage } from "@/db/message";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
     try {
         const body = await req.json()
-        await createMessage(body)
+        const newMessage = await createMessage(body)
 
-        return Response.json({ body }, { status: 200 })
+        return Response.json({ newMessage }, { status: 200 })
     } catch (error) {
-        return new NextRequest(`[CREATE_MESSAGE_ERR] ${error}`)
+        return new NextResponse(`[CREATE_MESSAGE_ERR] ${error}`)
     }
 }
